0d342f142bf5cf4ecbcf9a985c2009e79388a51e
SalasDelil <salasdelil8@gmail.com>
2023-05-14 20:52:54 +0300

    login authentication

--- data-cryptography/pages/login.tsx
+++ data-cryptography/pages/login.tsx
@@ -14,17 +14,17 @@ function Login() {
   } else {
     return (
       <div className="flex mx-auto justify-center h-screen">
-        <div className="w-1/2 bg-red-700 justify-center">
+        <div className="w-1/2 bg-blue-700 justify-center">
           <div className="pl-10 text-7xl text-white mt-40 mx-auto font-extrabold">
-            Welcome to DataCrypto!
+            Welcome to DataCipher!
           </div>
         </div>
         <div className="w-1/2 flex flex-col justify-center border rounded-md mx-auto px-4 py-5 shadow">
           <div className="text-xl font-bold mt-2">
-            Please Sign-in to continue !
+            Please Sign-in to continue!
           </div>
           <button
-            className="w-24 m-2 text-white bg-red-700 hover:bg-red-600 rounded-lg"
+            className="w-24 m-2 text-white bg-red-700 hover:bg-red-600 rounded-xl"
             onClick={() => signIn()}
           >
             Sign in
