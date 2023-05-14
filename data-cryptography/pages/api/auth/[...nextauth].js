d5dd32eb6cad3127f1225b74b93aa1684439454a
SalasDelil <salasdelil8@gmail.com>
2023-05-14 20:53:07 +0300

    next auth

--- data-cryptography/pages/api/auth/[...nextauth].js
+++ data-cryptography/pages/api/auth/[...nextauth].js
@@ -11,7 +11,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  secret: process.env.NEXTAUTH_SECRET
+  secret: process.env.JWT_SECRET
 }
 
 export default NextAuth(authOptions)
