090af85880a8827aae5db107fb688974a8538fa8
SalasDelil <salasdelil8@gmail.com>
2023-05-16 19:23:11 +0300

    added feature

--- data-cryptography/components/Layout/Footer.tsx
+++ data-cryptography/components/Layout/Footer.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="flex h-12 justify-center bg-red-800">
+    <footer className="flex h-12 justify-center bg-gray-600">
       <div className="flex-col">
-      <p className="font-normal text-white">For more contact:</p>
+      <p className="font-normal text-white">For more info:</p>
       <a
         className="font-semibold text-gray-100"
         href="https://github.com/SalasDelil"
